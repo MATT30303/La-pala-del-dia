0bb2d042fe360cac2106052f2da7509e1a27c35d
Matt <fabianbarzola2003@gmail.com>
2026-06-04 23:08:57 -0300

    feat: added modal responsive for phone

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -31,46 +31,40 @@ export default function Modal({
     { label: 'Mejor racha', value: stats.bestStreak },
   ];
   return (
-    <div className="w-full h-full font-Lato absolute top-0 left-0 backdrop-blur-[1px] flex flex-col justify-center items-center">
-      <div className="bg-[#1F1F1F] border border-[#7a7a7a] w-[40vw] h-[70vh] flex flex-col p-4 shadow-[0_4px_4px_rgb(0_0_0/_0.5)] justify-center  items-center rounded-md">
-        <div className="flex flex-col items-center relative flex-1 w-full">
+    <article className="w-full h-full font-Lato absolute top-0 left-0 backdrop-blur-[1px] flex flex-col justify-center items-center">
+      <div className="bg-[#1F1F1F] border border-[#7a7a7a] w-11/12 aspect-2/3 flex flex-col py-4 shadow-[0_4px_4px_rgb(0_0_0/_0.5)] justify-center  items-center rounded-md">
+        <section className="flex flex-col items-center relative w-full">
           <CloseMenu
-            className="z-10 absolute right-0 cursor-pointer text-stone-100 hover:scale-110 transition-transform hover:text-cyan-100"
+            className="z-10 absolute right-3 w-10 cursor-pointer text-stone-100 hover:scale-110 transition-transform hover:text-cyan-100"
             onClick={() => {
               onClose();
             }}
           />
           {isCorrect || turn >= 5 ? (
-            <div className="flex flex-col items-center h-3/12">
-              <span className="text-4xl uppercase tracking-wide mt-2 mb-4">
+            <div className="flex flex-col items-center h-10">
+              <span className="text-3xl uppercase tracking-wide">
                 {solution}
-              </span>
-              <span className="text-xl text-stone-200 text-center w-9/12">
-                {description ? description : ''}
               </span>
             </div>
           ) : null}
-        </div>
-        <div className="flex flex-col justify-center items-center flex-2">
-          <span className="text-3xl font-Lato text-stone-50 mb-4">
-            Estadisticas
-          </span>
-          <div className="flex w-full justify-around mt-4 ">
+        </section>
+        <section className="flex flex-col justify-center w-full items-center flex-2 gap-3">
+          <span className="text-2xl font-Lato text-stone-50">Estadisticas</span>
+          <div className="flex w-full justify-between">
             {STAT_ITEMS.map(({ label, value }) => (
-              <div key={label} className="flex flex-col  items-center w-24">
-                <span className="text-3xl mb-2 text-stone-100">{value}</span>
+              <div key={label} className="flex flex-col items-center w-full">
+                <span className="text-2xl mb-2 text-stone-100">{value}</span>
                 <span className="text-center font-Lato text-lg text-stone-100">
                   {label}
                 </span>
               </div>
             ))}
           </div>
-        </div>
+        </section>
 
-        <div className="w-full flex flex-col justify-center flex-3">
+        <section className="w-full flex flex-col justify-center items-center flex-3">
           <span className="text-3xl mb-4 block text-center">Distribución</span>
-
-          <div className="flex flex-col gap-2 px-8">
+          <div className="flex flex-col gap-2 w-11/12">
             {stats.distribution.map((count, i) => {
               const max = Math.max(...stats.distribution, 1);
               const width = (count / max) * 100;
@@ -90,8 +84,8 @@ export default function Modal({
               );
             })}
           </div>
-        </div>
+        </section>
       </div>
-    </div>
+    </article>
   );
 }
